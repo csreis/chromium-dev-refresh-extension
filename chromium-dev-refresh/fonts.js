2ec048b7c8ca166d5506addea59b9293a2f01043
Charles Reis <csreis@gmail.com>
2015-04-16 22:42:47 -0700

    Import fonts sooner.

--- chromium-dev-refresh/fonts.js
+++ chromium-dev-refresh/fonts.js
@@ -2,4 +2,4 @@
 // work from the content script's CSS file.
 var s = document.createElement("style");
 s.innerHTML = "@import url(https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700|Roboto:400,700,100);"
-document.body.appendChild(s);
+document.documentElement.appendChild(s);
